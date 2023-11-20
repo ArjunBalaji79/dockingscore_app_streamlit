704d6efa424ff45e75c8ce2a793c577843aed107
Arjun Balaji <95492772+ArjunBalaji79@users.noreply.github.com>
2023-11-20 07:08:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def main():
 
     if(selected_model=="GCN"):
       model_image = Image.open('GCNmodelflowchart.png')
-      st.image(model_image, caption='GCN Model Architecture',width=150)
+      st.image(model_image, caption='GCN Model Architecture',width=100)
     else:
       model_image = Image.open('EnhancedGCNmodelflowchart.png')
       st.image(model_image, caption='GCN+GAT Model Architecture', width=400)
